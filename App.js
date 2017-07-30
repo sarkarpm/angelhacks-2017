8c1100f9650ad4429b864eddfeb4a06984629746
Minh To <to.nguyet.minh@gmail.com>
2017-07-29 23:49:20 -0700

    all restaurants view, list view

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import LoginScreen from './frontend/containers/Login';
 import RegisterScreen from './frontend/containers/Register';
+import FoodView from './frontend/containers/FoodView';
+import AllProviders from './frontend/containers/AllProviders';
 
 export default StackNavigator({
   Login: {
@@ -10,7 +12,10 @@ export default StackNavigator({
   Register: {
     screen: RegisterScreen
   },
-  FoodView: {
+  "Food View": {
     screen: FoodView
+  },
+  "All Providers": {
+    screen: AllProviders
   }
 }, {initialRouteName: 'Login'});
