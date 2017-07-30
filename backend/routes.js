ef135a63534932c0509cc1df72b4fdbe783c634f
Priya Sarkar <pms40@duke.edu>
2017-07-30 09:11:30 -0700

    user profile

--- backend/routes.js
+++ backend/routes.js
@@ -55,6 +55,43 @@ router.get( '/providers/:providerId', ( req, res ) => {
     } )
 } )
 
+router.get( '/users/:userId', ( req, res ) => {
+    User.findById( req.params.userId, ( err, user ) => {
+        if ( err ) {
+            res.json( { success: false, message: err } );
+        }
+        else if ( !user ) {
+            res.json( { success: false, message: "No user found" } );
+        }
+        else {
+            res.json( { success: true, user: user } );
+        }
+    } )
+} )
+
+router.post('/newOrder', ( req, res ) => {
+  console.log('BEYONCE', req.body.userId)
+    User.findById( req.body.userId, ( err, user ) => {
+        if ( err ) {
+          console.log('BEY')
+            res.json( { success: false, message: err } );
+        }
+        else if ( !user ) {
+            res.json( { success: false, message: "No user found" } );
+        }
+        else {
+            user.orders.push({name: req.body.name,
+              quantity: req.body.quantity,
+              price: req.body.price,
+              unit: req.body.unit,
+              provider: req.body.provider
+            })
+            user.save();
+            res.json( { success: true, user: user } );
+        }
+    } )
+} )
+
 router.get( '/providers/:providerId/items', ( req, res ) => {
   console.log('PROVIDER ID', req.params.providerId);
     FoodProvider.findById( req.params.providerId )
@@ -103,8 +140,9 @@ router.post( '/providers/:providerId/new-item', ( req, res ) => {
         } );
 } )
 
-router.post('/providers/:providerId/remove-item', (req, res) => {
-  Item.findById(req.body.itemId, (err, item) => {
+router.get('/providers/:itemId/remove-item', (req, res) => {
+  console.log('taylor swift')
+  Item.findById(req.params.itemId, (err, item) => {
     if (err) {
       res.json({success: false, message: err});
     }
