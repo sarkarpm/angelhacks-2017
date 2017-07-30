5ed4e4d0605a60d34ba0178c4301efe268a0dcdd
teresaliu20 <teresali@usc.edu>
2017-07-30 09:51:53 -0700

    Added foodstyles

---
+++ frontend/foodStyles.js
@@ -0,0 +1,25 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+const win = Dimensions.get('window');
+const styles = StyleSheet.create({
+  foodView: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'flex',
+    width: win.width,
+    alignItems: 'center'
+  }
+  foodItem: {
+    width: win.width,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderBottomColor: '#4AC9BA',
+    borderBottomWidth: 2,
+    display: 'inline-block',
+    position: 'relative'
+  },
+})
+
+export default styles;
