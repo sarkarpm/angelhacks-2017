47e3cb5d792f15291a566f07d584ff7e48d1fafa
Minh To <to.nguyet.minh@gmail.com>
2017-07-29 17:16:11 -0700

    FoodView, FoodItem

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FoodView from './frontend/containers/FoodView';
+import styles from './frontend/styles.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <FoodView />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ frontend/components/FoodItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ListView,
+  Alert,
+  Button,
+  Dimensions,
+  AsyncStorage
+} from 'react-native';
+import styles from '../styles.js';
+
+class FoodItem extends React.Component {
+  render() {
+    return (
+      <View style={styles.foodItem}>
+        <Text>Item: {this.props.name}</Text>
+        <Text>Quantity: {this.props.quantity} {this.props.unit}</Text>
+        <Text>Price per item: ${this.props.price}</Text>
+        <TouchableOpacity style={styles.addButton}>
+          <View><Text style={{color: 'white'}}>+</Text></View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+export default FoodItem;
